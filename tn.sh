9a8878433abed35f7a0387d2db7940d45bf4b54a
Jan Dahl <jan@spacepope.dk>
2013-05-12 21:58:05 +0200

    Added comment on how to use a blank title as default

--- tn.sh
+++ tn.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
  
 # If running in screen, set trap to fall back to current hostname
+# If you want it to blank, replace ${HOSTNAME} with a space :-)
 if [ -n $STY ] ; then
   trap 'echo -n -e "\033k${HOSTNAME}\033\\"' 0 1 2 15
   echo -n -e "\033k$1\033\\"
