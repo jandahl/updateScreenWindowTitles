e201b37f02eb966c19507606ec8801854bd102d9
Jan Dahl <jan@spacepope.dk>
2013-05-13 11:20:46 +0200

    Moved default window name to a new var for easier access

--- ss.sh
+++ ss.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
  
 # If running in screen, set trap to fall back to current hostname
-# If you want it to blank, replace ${HOSTNAME} with a space :-)
+# If you want it to blank or be something static, replace ${HOSTNAME} :-)
+defaultTitle=${HOSTNAME}
+
 if [ -n $STY ] ; then
-  trap 'echo -n -e "\033k${HOSTNAME}\033\\"' 0 1 2 15
+  trap 'echo -n -e "\033k${defaultTitle}\033\\"' 0 1 2 15
   echo -n -e "\033k$1\033\\"
 fi
 /usr/bin/env ssh $*
