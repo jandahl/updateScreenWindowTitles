6b1c5d6d72666ac952cd7cf72dc79edc496fbd2d
Jan Dahl <jandahl@users.noreply.github.com>
2016-09-16 10:56:15 +0200

    Changed title to point to last argument
    instead of first

--- tn.sh
+++ tn.sh
@@ -3,9 +3,10 @@
 # If running in screen, set trap to fall back to current hostname
 # If you want it to blank or be something static, replace ${HOSTNAME} :-)
 defaultTitle=${HOSTNAME}
+currentTitle=${@: -1}
 
 if [ -n $STY ] || [ -n $TMUX ] ; then
   trap 'echo -n -e "\033k${defaultTitle}\033\\"' 0 1 2 15
-  echo -n -e "\033k$1\033\\"
+  echo -n -e "\033k${currentTitle}\033\\"
 fi
 /usr/bin/env telnet $*
